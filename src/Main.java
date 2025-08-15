bd8c2950d80cbb11944109bf326b955bbd8e9113
Daniela <a224204236@unison.mx>
2025-08-15 10:03:03 -0700

    Comienzo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        String archivo = "codigos_postales_hmo.csv";
+        String linea;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))){
+
+            while ((linea = br.readLine()) !=null){
+
+            }
+        }
+
 
         }
     }
